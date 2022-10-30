3959555ce3df7b5cfba44ccb928a1da9400f285c
Wajahat Jawaid <Wajahatjawaid@gmail.com>
2022-10-30 07:34:26 +0500

    Update centuryFromYear.java

--- centuryFromYear.java
+++ centuryFromYear.java
@@ -1,3 +1,8 @@
+/**
+ * Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100,
+ * the second - from the year 101 up to and including the year 200, etc.
+ */
+
 int solution(int year) {
     if(year % 100 == 0)
         return year / 100;
