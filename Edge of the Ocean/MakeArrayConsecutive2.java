426d780e8d0112ffd1ff088ea237f5697ca74b56
Wajahat Jawaid <Wajahatjawaid@gmail.com>
2022-10-30 07:41:23 +0500

    Create MakeArrayConsecutive2.java

---
+++ Edge of the Ocean/MakeArrayConsecutive2.java
@@ -0,0 +1,16 @@
+/**
+ * Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, 
+ each statue having an non-negative integer size. Since he likes to make things perfect, 
+ he wants to arrange them from smallest to largest so that each statue will be bigger than
+ the previous one exactly by 1. He may need some additional statues to be able to accomplish that. 
+ Help him figure out the minimum number of additional statues needed.
+ */
+
+int solution(int[] statues) {
+    Arrays.sort(statues);
+    
+    int range = (statues[statues.length - 1] - statues[0]) + 1;
+    
+    
+    return range - statues.length;
+}
